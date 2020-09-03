57b38a1dc3b417e0eaa9e7ac5f0585ced2a15f0f
Viktor Shcheplyagin <silverman995@gmail.com>
2020-09-04 00:52:24 +0300

    Education component created

---
+++ src/pages/Home/components/Education/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Flex, Text } from '@chakra-ui/core';
+
+const Education: React.FC = () => {
+  return (
+    <Flex direction="column" align="center">
+      <Text>Odessa National Polytechnic University</Text>
+      <Text>2015 - 2019</Text>
+      <Text>System Engineering</Text>
+    </Flex>
+  )
+};
+
+export default Education;
