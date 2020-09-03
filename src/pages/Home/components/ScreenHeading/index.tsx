7312864d498c788920c81d6b35f2e6b2176c8a8a
Viktor Shcheplyagin <silverman995@gmail.com>
2020-09-03 22:51:43 +0300

    ScreenHeading component created

---
+++ src/pages/Home/components/ScreenHeading/index.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Flex, Text, Divider } from '@chakra-ui/core';
+
+
+const dividerStyle: any = {
+  width: "full",
+  mx: [4]
+};
+
+
+const ScreenHeading: React.FC<{ children: string }> = ({ children }) => {
+  return (
+    <Flex align="center">
+      <Divider {...dividerStyle} />
+      <Text
+        fontSize="xl"
+        fontWeight="semibold"
+      >
+        {children}
+      </Text>
+      <Divider {...dividerStyle} />
+    </Flex>
+  )
+};
+
+export default ScreenHeading;
