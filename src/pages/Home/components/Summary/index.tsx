1afb93665d2e86fc600d1f46b2658ff73b45aaa3
Viktor Shcheplyagin <silverman995@gmail.com>
2020-09-04 00:53:44 +0300

    Summary component created

---
+++ src/pages/Home/components/Summary/index.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Text } from '@chakra-ui/core';
+
+const Summary: React.FC = () => {
+  return (
+    <>
+      <Text>
+        I like to develop a frontend, using React, Redux and TypeScript. Also, I'm very interested in backend development (NodeJS), mobile and desktop dev (React Native, Electron).
+      </Text>
+      <Text>
+        My current aim is full-stack web development.
+      </Text>
+    </>
+  )
+};
+
+export default Summary;
