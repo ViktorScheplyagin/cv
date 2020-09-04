e52ef095896963ddca417bab4d00c99f3c6953ac
Viktor Shcheplyagin <silverman995@gmail.com>
2020-09-04 20:47:31 +0300

    edited Summary's contend

--- src/pages/Home/components/Summary/index.tsx
+++ src/pages/Home/components/Summary/index.tsx
@@ -1,16 +1,51 @@
 import React from 'react';
-import { Text } from '@chakra-ui/core';
+import { Text, Box, } from '@chakra-ui/core';
+import TechnologyLabel from './TechnologyLabel';
+
 
 const Summary: React.FC = () => {
   return (
-    <>
+    <Box mx="auto" w={["80%", "90%"]}>
       <Text>
-        I like to develop a frontend, using React, Redux and TypeScript. Also, I'm very interested in backend development (NodeJS), mobile and desktop dev (React Native, Electron).
+        I like to develop a frontend, using
+        <TechnologyLabel
+          name="React"
+          icon="react"
+          description="A JavaScript library for building user interfaces"
+        />,
+        <TechnologyLabel
+          name="Redux"
+          icon="redux"
+          description="A Predictable State Container for JS Apps"
+        /> and
+        <TechnologyLabel
+          name="TypeScript"
+          icon="typescript"
+          description="TypeScript extends JavaScript by adding types"
+        />.
+
+        Also, I'm very interested in backend development
+        (<TechnologyLabel 
+          name="NodeJS" 
+          icon="nodejs" 
+          description="Node.js® is a JavaScript runtime built on Chrome's V8 JavaScript engine"
+        />),
+        mobile and desktop dev
+        (<TechnologyLabel
+          name="React Native"
+          icon="react"
+          description="A framework for building native apps using React"
+        />,
+        <TechnologyLabel
+          name="Electron"
+          icon="electron"
+          description="Build cross-platform desktop apps with JavaScript, HTML, and CSS"
+        />).
       </Text>
-      <Text>
+      <Text mt={[8]}>
         My current aim is full-stack web development.
       </Text>
-    </>
+    </Box>
   )
 };
 
