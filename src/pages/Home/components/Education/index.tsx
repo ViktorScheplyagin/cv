24f9dee75a7351ca585757b47e3b84b9205317fb
Viktor Shcheplyagin <silverman995@gmail.com>
2020-09-04 20:43:27 +0300

    added university logo

--- src/pages/Home/components/Education/index.tsx
+++ src/pages/Home/components/Education/index.tsx
@@ -1,13 +1,26 @@
 import React from 'react';
-import { Flex, Text } from '@chakra-ui/core';
+import logoOnpu from 'assets/logo_onpu.png';
+import { SimpleGrid, Flex, Text, Image, Box } from '@chakra-ui/core';
 
 const Education: React.FC = () => {
   return (
-    <Flex direction="column" align="center">
-      <Text>Odessa National Polytechnic University</Text>
-      <Text>2015 - 2019</Text>
-      <Text>System Engineering</Text>
-    </Flex>
+    <SimpleGrid columns={[1, 2]} textTransform="uppercase">
+      <Flex direction="column" align="center" justify="space-between">
+        <Image size="50px" src={logoOnpu} mb={4} />
+        <Text fontWeight="semibold">
+          Odessa National Polytechnic University
+        </Text>
+      </Flex>
+
+      <Flex direction="column" align="center" justify="center">
+        <Box>
+          <Text fontWeight="semibold">
+            System Engineering
+          </Text>
+          <Text>2015 - 2019</Text>
+        </Box>
+      </Flex>
+    </SimpleGrid>
   )
 };
 
