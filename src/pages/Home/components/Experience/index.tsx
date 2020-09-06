5ad8ec075af1ed2566206525f930a6d277dd1e93
Viktor Shcheplyagin <silverman995@gmail.com>
2020-09-06 13:22:45 +0300

    team members quantity moven below a company name; added Portfolio

--- src/pages/Home/components/Experience/index.tsx
+++ src/pages/Home/components/Experience/index.tsx
@@ -1,24 +1,53 @@
 import React from 'react';
-import { Flex, SimpleGrid, Heading, Text } from '@chakra-ui/core';
+import { Flex, SimpleGrid, Text, Icon, Box } from '@chakra-ui/core';
+import Portfolio from './Portfolio';
+
 
 const Experience: React.FC = () => {
   return (
-    <SimpleGrid columns={[1, null, 2]} mx={[4, null, 6]} spacing={[4, null, 0]}>
-      <Flex direction="column" align="center" justify="center">
-        <Heading>KikiDevelopment</Heading>
-        <Text
-          color="gray.600"
-          fontWeight="semibold"
-          fontSize="lg"
-        >
-          Junior Front-End developer
-        </Text>
+    <SimpleGrid
+      columns={[1, null, 2]}
+      mx={[4, null, 6]}
+      spacing={[4, null, 0]}
+    >
+      <Flex justify="center" mb={[20, 0]}>
+        <Icon name="kikidev" size="50px" mr={1} />
+
+        <Box textTransform="uppercase">
+          <Text fontSize="2xl" fontWeight="bold">
+            KikiDevelopment
+          </Text>
+          <Text mb={2} fontSize="sm" fontWeight="semibold">
+            5 team members
+          </Text>
+          <Text fontSize="sm">
+            May, 2018 - Aug, 2020 
+          </Text>
+        </Box>
       </Flex>
 
-      <Flex align="center" justify="center">
-        <Text textAlign={["center", null, "left"]}>
-          With which projects we worked, provide short description for each project
-        </Text>
+      <Flex
+        direction="column"
+        align="center"
+        justify="center"
+      >
+        <Box 
+          textAlign="left" 
+          w="100%"
+          color="gray.600"
+          fontWeight="semibold"
+          fontSize="md"
+          textTransform="uppercase"
+        >
+          <Text mb={1}>
+            Junior Front-End developer
+          </Text>
+          <Text mb={2} fontSize="sm" color="gray.800">
+            Projects:
+          </Text>
+        </Box>
+
+        <Portfolio />
       </Flex>
     </SimpleGrid>
   )
