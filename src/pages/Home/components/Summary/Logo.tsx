e1fde3c07b5fd05579d0fcb062dba883e7372796
Viktor Shcheplyagin <silverman995@gmail.com>
2020-09-06 13:24:57 +0300

    Logo:  fixed look, when is no icon provided

--- src/pages/Home/components/Summary/Logo.tsx
+++ src/pages/Home/components/Summary/Logo.tsx
@@ -1,18 +1,26 @@
 import React from 'react';
-import { Flex, Text, Icon } from '@chakra-ui/core';
+import { Flex, Text, Icon, Box } from '@chakra-ui/core';
 
 
 type LogoProps = {
   name: string;
-  icon?: string;
+  icon?: any;
   size?: number;
 };
 
 
 const Logo: React.FC<LogoProps> = ({ name, icon, size = 14}) => {
+  const iconSize = `${size + 2}px`;
+
   return (
     <Flex align="center" justify="space-between">
-      <Icon size={`${size + 2}px`} name={icon} mr={1} />
+      {
+        icon === undefined
+        ? <Box w={0} h={iconSize} />
+        : typeof icon === "string"
+          ? <Icon size={iconSize} name={icon} mr={1} />
+          : <Box as={icon} size={iconSize} mr={1} />
+      }
       <Text fontWeight={`${size}px`}>{name}</Text>
     </Flex>
   )
