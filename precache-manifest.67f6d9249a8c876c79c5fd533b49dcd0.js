2d0ff8c55f328a3f4fa36234fc7385e8597e831e
Viktor Shcheplyagin <silverman995@gmail.com>
2020-09-06 22:17:10 +0300

    Updates

---
+++ precache-manifest.67f6d9249a8c876c79c5fd533b49dcd0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d3f800a02329050b49be4d8b3adade4",
+    "url": "/cv/index.html"
+  },
+  {
+    "revision": "f43311764573a5b600e0",
+    "url": "/cv/static/js/2.9882283a.chunk.js"
+  },
+  {
+    "revision": "d2966845b94a3318bf32eecc7af8015d",
+    "url": "/cv/static/js/2.9882283a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "57560de49fccc4d34327",
+    "url": "/cv/static/js/main.ed48d32c.chunk.js"
+  },
+  {
+    "revision": "9e0e921495960402e4a4",
+    "url": "/cv/static/js/runtime-main.c17789b0.js"
+  },
+  {
+    "revision": "f4bde314cb0b50b9a83d123ffc7ccd2e",
+    "url": "/cv/static/media/logo_onpu.f4bde314.png"
+  },
+  {
+    "revision": "05d90f30da71316f448ee5c503247fe9",
+    "url": "/cv/static/media/my_photo.05d90f30.jpg"
+  }
+]);
