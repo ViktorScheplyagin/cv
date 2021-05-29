d34ffb463338a842ab682883d016e2a10b75f61f
Shcheplyagin, Viktor <v.shcheplyagin@itransition.com>
2021-05-30 02:07:45 +0300

    update skills

--- src/pages/Home/components/Skills/SkillsList.tsx
+++ src/pages/Home/components/Skills/SkillsList.tsx
@@ -10,25 +10,10 @@ type SkillsListProps = {
   skills: Skill[];
 };
 
-export const SkillsList: React.FC<SkillsListProps> = ({ skills }) => {
-  const isTable = skills.some((skill) => Object.keys(skill).length > 1);
-
-  const mapSkillToTable = (skill: Skill, idx: number) => (
-    <ListItem key={idx}>
-      <table width='100%' style={{ borderCollapse: 'collapse' }}>
-        <tr style={{ borderBottom: '1px solid' }}>
-          <td width='50%'>{skill.name}</td>
-          <td width='50%'>{skill.experience}</td>
-        </tr>
-      </table>
-    </ListItem>
-  );
-
-  const mapSkillToLi = (skill: Skill, idx: number) => <ListItem key={idx}>{skill.name}</ListItem>;
-
-  return (
-    <List styleType={isTable ? 'none' : 'disk'}>
-      {skills.map(isTable ? mapSkillToTable : mapSkillToLi)}
-    </List>
-  );
-};
+export const SkillsList: React.FC<SkillsListProps> = ({ skills }) => (
+  <List styleType='disk'>
+    {skills.map((skill: Skill, idx: number) => (
+      <ListItem key={idx}>{skill.name}</ListItem>
+    ))}
+  </List>
+);
